adc56408a4c959ae7ee1dfd391290732e88d79ec
Svitlana <svitlana.grytsai@gmail.com>
2024-04-29 22:54:52 +0200

    feat: implement function getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const nextId = id;
+    id += 1;
+    return nextId;
+  };
 }
 
 module.exports = {
